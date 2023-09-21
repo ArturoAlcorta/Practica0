eb484588f090ce8f411f678c3f0e5d3ae6a78d5c
ArturoAlcorta <arturo.alcorta@estudiante.uam.es>
2023-09-21 19:32:51 +0200

    second commit

---
+++ lib.js
@@ -0,0 +1,19 @@
+export function hello(){
+
+    // para añadir comentarios asi
+    /* o asi para los bloques */
+
+    const hello = 'hola'
+
+    return hello + ' mundo!'
+
+}
+
+console.log(hello())
+
+function miFactorial(n){
+
+
+
+
+}
